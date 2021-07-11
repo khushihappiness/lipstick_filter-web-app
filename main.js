670503a384d53c59984b70e51c71881fe93069d3
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-07-11 12:57:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,13 +9,32 @@ function setup()
     video=createCapture(VIDEO)
     video.hide();
 
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded()
+{
+    console.log(" The poseNet model is loaded")
 }
 
 function draw()
 {
-
+    mage(video,0,0, 400,400)
 }
 
+function gotPoses(results)
+{
+if(results.length>0)
+{
+    console.log(results)
+    console.log("The lips x=" + results[0].pose.lips.x);
+    console.log("The lips y=" + results[0].pose.lips.y);
+
+
+}
+}
 function take_snapshot()
 {
     save("lipstick_filter.png")
